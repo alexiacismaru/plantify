59378c109a62bda2ef25080d5971af372b2936da
Alexia <cismaru.mariaalexia@student.kdg.be>
2024-01-23 16:35:04 +0100

    product's website

---
+++ plantifywebsite/js/plantContainer.js
@@ -0,0 +1,32 @@
+alert("hello")
+let data = [
+  { "plant": "plant1" ,
+    "humidity":42,
+      "temperature":27.9,
+      "brightness":63,
+      "moisture":90
+  },
+  {
+    "plant": "plant2" ,
+    "humidity":45,
+      "temperature":20.3,
+      "brightness":54,
+      "moisture":34
+  },
+  {
+    "plant": "plant3" ,
+    "humidity":40,
+      "temperature":19.2,
+      "brightness":49,
+      "moisture":67
+  },
+  {
+    "plant": "plant4" ,
+    "humidity":70,
+      "temperature":14,
+      "brightness":35,
+      "moisture":22
+  }
+]
+
+const plants = document.getElementsByClassName()
